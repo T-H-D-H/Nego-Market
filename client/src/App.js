97f2c14cfd051cd616fc1b1f600e779d834c356e
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2023-01-29 18:57:25 +0900

    feat: 로그인 로직 완료

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import SignUP from './pages/SignUp';
 import Home from './pages/Home';
+import Login from './pages/Login';
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/signup" element={<SignUP/>}/>
+        <Route path="/login" element={<Login/>}/>
       </Routes>
       
     </BrowserRouter>
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,52 @@
+import './../styles/Login.css'
+import NavigationBar from "../components/Navigation";
+import { useState } from 'react';
+import { json } from 'react-router';
+
+export default function Login() {
+    const [id,setId] =useState("");
+    const [pw,setPw] =useState("");
+    const port = process.env.REACT_APP_PORT
+    const userLogin = async()=>{
+       
+        const inputs = {
+            email : id,
+            password : pw
+        }
+
+        const data = JSON.stringify(inputs)
+        
+
+        try{
+            const res = await fetch(port+'/api/user/login',{
+                method: "POST",
+                headers: {
+                  "Content-Type": "application/json",
+                },
+                body: data
+              });
+              if(!res.ok){
+                throw new Error("로그인 과정중에 문제가 생김")
+              }
+              const isLogined = await res.json();
+              const token = JSON.stringify(isLogined.token)
+              sessionStorage.setItem("token",token)
+              alert("로그인 성공했습니다!")
+              window.location.href="/"
+              console.log(token)
+        }catch(err){
+           alert("일치하는 회원이 없습니다. 다시 확인해주세요.")
+        }
+        
+    }
+
+    return <div>
+        <NavigationBar></NavigationBar>
+        <div className='inputs'>
+            <input placeholder="이메일" type="text" onChange={(e)=>{setId(e.target.value)}}/>
+            <input placeholder="비밀번호" type="text" onChange={(e)=>{setPw(e.target.value)}}/>
+            <input type="button" value="로그인 하기" onClick={userLogin}/>
+        </div>
+
+    </div>;
+}
