020710a2d49a6e38f238d317d2a176246bd4f3a4
DESKTOP-JH9P8S7\taehun <ekdh0858@naver.com>
2023-03-19 21:16:14 +0900

    #5 fix : 실제 API연동 시키기

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -2,27 +2,96 @@ import NavigationBar from "../components/Navigation";
 import style from "./../styles/Home.module.css";
 // import mainContent from "./mainContents.json";
 import { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 
 export default function Home() {
 	const port = process.env.REACT_APP_PORT;
-	const [mainContent,setMainContent] =useState();
-	const loadMainContent = async ()=>{
+	const [mainContent, setMainContent] = useState();
+	const [si, setSi] = useState();
+	const [gu, setGu] = useState();
+	const [dong, setDong] = useState();
+	const loadMainContent = async () => {
 		// 메인 콘텐츠들을 배열 형태로 가져오는 것.
-		// 로그인 안했을때 
-		const res = await fetch(port + "/api/products",{
-			method:"GET",
-			headers:{
+		// 로그인 안했을때 , 했을때
+		const token = JSON.parse(sessionStorage.getItem("token"));
+
+		// const url = "";
+
+		// if (token) {
+		// 	url = "/api/products/my-address";
+		// } else {
+		// 	url = "/api/products";
+		// }
+		let res = [];
+		token?
+		 res = await fetch(port + "/api/products/my-address", {
+			method: "GET",
+			headers: {
 				"Content-Type": "application/json",
-			}
-		});
+				Authorization: "Bearer " + token,
+			},
+		}):res = await fetch(port + "/api/products", {
+			method: "GET",
+			headers: {
+				"Content-Type": "application/json",
+				// Authorization: "Bearer " + token,
+			},
+		})
 		const result = await res.json();
 
 		setMainContent(result);
-	}
+	};
+	// 지역 선택 했을때, 구 선택 했을때, 동 선택했을때,
+	const setMainContentByAddress = async (region,selectedRegion) => {
+		if (region=="dong") {
+			// 동으로 선택된게 있을때
+			console.log("동을 선택했을때",si,gu,dong)
+			const res = await fetch(
+				port + "/api/products/" + si + "/" + gu + "/" + selectedRegion,
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			const result = await res.json();
+			setMainContent(result);
+		}else if(region=="gu"){
+			// 구로 선택된게 있을때
+			console.log("구를 선택했을때",si,gu)
+			const res = await fetch(
+				port + "/api/products/" + si + "/" + selectedRegion ,
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			const result = await res.json();
+			setMainContent(result);
+		}else if(region=="si"){
+			// 시로 선택된게 있을때
+			console.log("시을 선택했을때",si)
+			const res = await fetch(
+				port + "/api/products/" + selectedRegion ,
+				{
+					method: "GET",
+					headers: {
+						"Content-Type": "application/json",
+					},
+				}
+			);
+			const result = await res.json();
+			console.log("시에 선택 됬을때 : ",result)
+			setMainContent(result);
+		}
+	};
 
-	useEffect(()=>{
+	useEffect(() => {
 		loadMainContent();
-	},[mainContent])
+	}, []);
 
 	const [getData, setGetData] = useState();
 	const regionLoad = async () => {
@@ -40,10 +109,6 @@ export default function Home() {
 		regionLoad();
 	}, []);
 
-	const [si, setSi] = useState();
-	const [gu, setGu] = useState();
-	const [dong, setDong] = useState();
-
 	const [guData, setGuData] = useState();
 	const loadGuData = async (e) => {
 		const data = await fetch(port + "/api/address/gu?si=" + e, {
@@ -59,10 +124,9 @@ export default function Home() {
 	};
 
 	const [dongData, setDongData] = useState();
-	const loadDongData = async (e) => {
-		
+	const loadDongData = async (gu) => {
 		const data = await fetch(
-			port + "/api/address/dong?si=" + si + "&gu=" + e,
+			port + "/api/address/dong?si=" + si + "&gu=" + gu,
 			{
 				method: "GET",
 				headers: {
@@ -80,107 +144,120 @@ export default function Home() {
 		<div>
 			<NavigationBar></NavigationBar>
 			<div className={style.mainTitle}>지역의 인기매물</div>
-			
 
 			<div className={style.content}>
-			<div className={style.selectLocation}>
-				<span>인기 매물 </span>
-				<div className={style.select}>
-					<select
-						onChange={async (e) => {
-							setSi(e.target.value);
-							setDongData("")
-							loadGuData(e.target.value);
-						}}
-					>
-						<option value="">지역을 선택하세요</option>
-						{getData ? (
-							<>
-								{getData.map((e) => {
-									return (
-										<option key={e} value={e}>
-											{e}
-										</option>
-									);
-								})}
-							</>
-						) : (
-							<>no</>
-						)}
-					</select>
-					<select
-						onChange={(e) => {
-							setGu(e.target.value);
-							loadDongData(e.target.value);
-						}}
-					>
-						<option value="">동네를 선택하세요</option>
-						{guData ? (
-							<>
-								{guData.map((e) => {
-									return (
-										<option key={e} value={e}>
-											{e}
-										</option>
-									);
-								})}
-							</>
-						) : (
-							<></>
-						)}
-					</select>
-					<select
-						onChange={(e) => {
-							setDong(e.target.value);
-						}}
-					>
-						<option value="">동을 선택하세요</option>
-						{dongData ? (
-							<>
-								{dongData.map((e) => {
-									return (
-										<option key={e} value={e}>
-											{e}
-										</option>
-									);
-								})}
-							</>
-						) : (
-							<></>
-						)}
-					</select>
+				<div className={style.selectLocation}>
+					<span>인기 매물 </span>
+					<div className={style.select}>
+						<select
+							onChange={async(e) => {
+								setSi(e.target.value);
+								setDongData("");
+								loadGuData(e.target.value);
+								setMainContentByAddress("si",e.target.value);
+								console.log("시 바꿧을때")
+							}}
+							value={si}
+						>
+							<option value="">지역을 선택하세요</option>
+							{getData ? (
+								<>
+									{getData.map((e) => {
+										return (
+											<option key={e} value={e}>
+												{e}
+											</option>
+										);
+									})}
+								</>
+							) : (
+								<></>
+							)}
+						</select>
+						<select
+							onChange={(e) => {
+								setGu(e.target.value);
+								loadDongData(e.target.value);
+								setMainContentByAddress("gu",e.target.value);
+								setMainContentByAddress();
+							}}
+						>
+							<option value="">동네를 선택하세요</option>
+							{guData ? (
+								<>
+									{guData.map((e) => {
+										return (
+											<option key={e} value={e}>
+												{e}
+											</option>
+										);
+									})}
+								</>
+							) : (
+								<></>
+							)}
+						</select>
+						<select
+							onChange={(e) => {
+								setDong(e.target.value);
+								setMainContentByAddress();
+								setMainContentByAddress("dong",e.target.value);
+							}}
+						>
+							<option value="">동을 선택하세요</option>
+							{dongData ? (
+								<>
+									{dongData.map((e) => {
+										return (
+											<option key={e} value={e}>
+												{e}
+											</option>
+										);
+									})}
+								</>
+							) : (
+								<></>
+							)}
+						</select>
+					</div>
 				</div>
-			</div>
-				{mainContent?mainContent.map((content) => {
-					return (
-						<article className={style.product} key={content.id}>
-							<a href="" className={style.product_link}>
-								<div className={style.product_img}>
-									<img
-										src={content.img}
-										alt={content.title}
-										className={style.img}
-									/>
-								</div>
-								<div className={style.product_desc}>
-									<h2 className={style.product_title}>
-										{content.title}
-									</h2>
-									<div className={style.product_price}>
-										{content.price}
+				{mainContent ? (
+					mainContent.map((content) => {
+						return (
+							<article className={style.product} key={content.id}>
+								<Link
+									to={`/product/${content.id}`}
+									className={style.product_link}
+								>
+									<div className={style.product_img}>
+										<img
+											src={content.img}
+											alt={content.title}
+											className={style.img}
+										/>
 									</div>
-									<div className={style.product_region}>
-										{content.region}
+									<div className={style.product_desc}>
+										<h2 className={style.product_title}>
+											{content.title}
+										</h2>
+										<div className={style.product_price}>
+											{content.price}
+										</div>
+										<div className={style.product_region}>
+											{content.region}
+										</div>
+										<div className={style.product_count}>
+											<span>관심 {content.good}</span> ∙{" "}
+											<span>채팅 {content.chat}</span>
+										</div>
 									</div>
-									<div className={style.product_count}>
-										<span>관심 {content.good}</span> ∙{" "}
-										<span>채팅 {content.chat}</span>
-									</div>
-								</div>
-							</a>
-						</article>
-					);
-				}):<></>}
+								</Link>
+							</article>
+						);
+					})
+				) : (
+					<></>
+				)}
 			</div>
 		</div>
 	);
